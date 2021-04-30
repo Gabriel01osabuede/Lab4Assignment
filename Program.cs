15517fbcb29e0eb87f6f5112f328baaf8d5770a6
Gabriel01osabuede <buedgabby@gmail.com>
2021-04-30 18:40:30 +0100

    Added user validation

--- Program.cs
+++ Program.cs
@@ -21,7 +21,6 @@ namespace Lab4Assignment
             studentCgpaCalculator myStudentCgpaCalculator = new studentCgpaCalculator();
             myStudentCgpaCalculator.acceptInputAndStoreInList();
             myStudentCgpaCalculator.cgpaCalculation();
-            myStudentCgpaCalculator.cgpaPercentageCalculation();
         }
     }
 
@@ -29,27 +28,38 @@ namespace Lab4Assignment
    {
        string _studentName;
        int _score;
+       string studentScore;
        List<double> _studentScoreList = new List<double>();
        List<double> base10ScoreStoredInList = new List<double>();
        double _cgpaScore;
        double cgpaPercentage;
-        
-
+       
+       
        public void acceptInputAndStoreInList()
        {
             string reply = "NO";
+            bool invalidInput = false;
            Console.WriteLine("Welcome To the student Cgpa Calculator");
            Console.WriteLine("We need your name and scores.");
            Console.WriteLine("Please input Your Name\n");
            _studentName = Console.ReadLine();
           
            //creating a list to store the scores coming in by the user.
-           
+           do{
            Console.WriteLine("And scores to calculate your Cgpa and Cgpa Percentage.");
-           
            string studentScore =  Console.ReadLine();
-           _score = Convert.ToInt32(studentScore);
-           _studentScoreList.Add(_score);
+           if(Int32.TryParse(studentScore, out _score))
+           {
+                _score = Convert.ToInt32(studentScore);
+                _studentScoreList.Add(_score);
+                break;
+           }else
+           {
+               Console.WriteLine("Invalid Score Provided..Input Again");
+               invalidInput = true;
+           }
+           }while(invalidInput);
+           
            
            do{
             Console.WriteLine("Would you like to add More Scores. YES OR NO");
@@ -78,13 +88,9 @@ namespace Lab4Assignment
            double[] base10ScoreArray = base10ScoreStoredInList.ToArray();
            double totalScore = base10ScoreArray.Sum();
            _cgpaScore = totalScore / base10ScoreArray.Length;
-           Console.WriteLine($"[Mr/Mrs/Miss] : {_studentName}\nYour Cgpa score is : {_cgpaScore}.");       
-       }
-
-       public void cgpaPercentageCalculation()
-       {
-           cgpaPercentage = _cgpaScore * 9.5;
-           Console.WriteLine($"Your cgpa percentage = {cgpaPercentage:n2}%\nThank you.");
+            cgpaPercentage = _cgpaScore * 9.5;
+           Console.WriteLine($"[Mr/Mrs/Miss] : {_studentName}\nYour Cgpa score is : {_cgpaScore:F2}.");       
+           Console.WriteLine($"Your cgpa percentage = {cgpaPercentage:F2}%\nThank you.");
        }
     }
 }
